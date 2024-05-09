f09a408589870e7292785c2b2ec5337a8f00c713
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-05-09 14:29:31 +0500

    commit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 function calculator(a) {
-    var input = document.getElementById("input").value +=a
-    console.log(input);
+    var input = document.getElementById("input").value += a; 
+    console.log(input); 
+    var multiply = eval(input); 
+    let result = document.getElementById("input").value = multiply
+    console.log(result);
     
 }
+
+
 function lastDelete(a) {
-    var current = document.getElementById("input").value 
-    document.getElementById("input").value = current.slice(0,-1)
+    var current = document.getElementById("input").value;
+    document.getElementById("input").value = current.slice(0,-1); 
 }
+
 function clearAll() {
-    var input = document.getElementById("input").value = " "
+    var input = document.getElementById("input").value = " "; 
 }
