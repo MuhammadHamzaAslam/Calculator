9f6662a99ed7ec8d1fe05f74b11758e407c92fbf
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-05-29 15:42:18 +0500

    done

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+var input = ""; // Declare input globally
+
 function calculator(a) {
-    var input = document.getElementById("input").value += a; 
-    console.log(input); 
-    var multiply = eval(input); 
-    let result = document.getElementById("input").value = multiply
-    console.log(result);
-    
+    input += a; 
+    document.getElementById("input").value = input; 
 }
 
+function calculate(b) {
+    if (b == "=") {
+        var result = eval(input); 
+        console.log(result);
+        document.getElementById("input").value = result;
+        input = ""; 
+    }
+}
 
-function lastDelete(a) {
+function lastDelete() {
     var current = document.getElementById("input").value;
     document.getElementById("input").value = current.slice(0,-1); 
 }
 
 function clearAll() {
-    var input = document.getElementById("input").value = " "; 
+    input = ""; 
+    document.getElementById("input").value = ""; 
 }
